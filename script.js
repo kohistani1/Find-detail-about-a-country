3255c3bd82d40a78f3814969695593e148c72c49
= <=>
2021-10-23 12:21:06 -0700

    day-2 learning

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 'use strict';
 
 const inputEl = document.querySelector('.btn-country');
+const images = document.querySelector('.images');
 const countriesContainer = document.querySelector('.countries');
 const btnEl = document.querySelector('.btn');
 ///////////////////////////////////////
 
-// const request = new XMLHttpRequest();
-// request.open('GET', 'https://restcountries.eu/rest/v2/name/Colombia');
-// request.send();
-
-// request.addEventListener('load', () => {
-//   const data = request.responseText;
-//   console.log('logging data', data);
-// });
 const renderEl = (country, className) => {
   const html = `
   <article class="country ${className}">
@@ -26,25 +19,17 @@ const renderEl = (country, className) => {
       <p class="country__row"><span>💰</span>${country.currencies[0].name}</p>
     </div>
   </article>`;
-      countriesContainer.insertAdjacentHTML('beforeend', html);
-      countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 };
-const renderErr = (err)=>{
-  countriesContainer.insertAdjacentText('afterend',`${err.message} 💥💥💥`)
+const renderErr = err => {
+  countriesContainer.insertAdjacentText('afterend', `${err.message} 💥💥💥`);
   console.log(`${err.message}`);
-}
+};
 
 const getInfo = () => {
   const userChoice = inputEl.value;
   if (!userChoice) return alert('please enter any Name');
-
-  // const request = new XMLHttpRequest();
-  // request.open('GET', `https://restcountries.com/v2/name/${userChoice}`);
-  // request.send();
-
-  // request.addEventListener('load', () => {
-  //   const [country] = JSON.parse(request.responseText);
-  //   console.log(country);
 
   fetch(`https://restcountries.com/v2/name/${userChoice}`)
     .then(response => response.json())
@@ -65,7 +50,85 @@ const getInfo = () => {
     .then(data => {
       renderEl(data, 'neighbour');
       console.log(data);
-})}
+    });
+};
+
+const CreateImg = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      images.prepend(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('img not found'));
+    });
+  });
+};
+
+
+const getLocation = function () {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getLocation().then(pos => console.log(pos));
+
+btnEl.addEventListener('click', getInfo);
+btnEl.addEventListener('click', getLocation);
+
+/////////////////////////////
+const wait = function (seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('I waited for 1 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 2 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 3 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 4 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 5 second');
+    return wait(1);
+  });
+
+const lotteryPromis = new Promise((resolve, reject) => {
+  console.log('Draw is happining');
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve('You win 😎');
+    } else {
+      reject(new Error('You Lost the Bet'));
+    }
+  }, 2000);
+});
+lotteryPromis.then(res => console.log(res)).catch(err => console.log(err));
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${userChoice}`);
+// request.send();
+
+// request.addEventListener('load', () => {
+//   const [country] = JSON.parse(request.responseText);
+//   console.log(country);
+
 // fetch(`https://geek-jokes.sameerkumar.website/api?format=json`)
 //   .then(response => response.json())
 //   .then(data => {
@@ -85,5 +148,3 @@ const getInfo = () => {
 //   .then(res => res.json())
 //   .then(d => console.log('3rd joke: ', d.joke))
 //   .catch(err => console.error(`could not load data ${err.message}`));
-
-btnEl.addEventListener('click', getInfo);
